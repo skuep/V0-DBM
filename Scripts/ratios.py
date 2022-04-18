1584acc72f628495c06235f8b358e63ead62715a
Simon Kueppers <simon.kueppers@skueppers.de>
2022-04-18 12:17:27 +0200

    Improved belt ratio calculation script to remove duplicates (among other things)

--- Scripts/ratios.py
+++ Scripts/ratios.py
@@ -1,23 +1,50 @@
 from fractions import Fraction
+from typing import Iterable, NamedTuple
 
-pulleys = range(22, 33)
-limits = range(2, 9)
+class BeltRatio(NamedTuple):
+    a: int
+    b: int
+    c: int
+    d: int
+
+    @property
+    def pulleys(self) -> Iterable[Fraction]:
+        return (Fraction(self.a, self.b), Fraction(self.c, self.d))
+
+    @property
+    def ma(self) -> Fraction:
+        return 2 / (1 - self.pulleys[0] * self.pulleys[1])
+
+    def __eq__(self, other):
+        me = self.pulleys
+        you = other.pulleys
+        return True if (me == you) or (me == you[::-1]) else False
+
+    def __str__(self):
+        return f'1:{self.ma} [{self.a}/{self.b}{"*" if self.a==self.b else ""}, {self.c}/{self.d}{"*" if self.c==self.d else ""}]'
+
+pulleys = range(24, 33)
+limits = [2, 4, 5, 8, 10, 16, 20]
 
 print(f'pulleys: {list(pulleys)}')
 print(f'limits: {list(limits)}')
 
-for a_pulley in pulleys:
-    for b_pulley in pulleys:
-        for c_pulley in pulleys:
-            for d_pulley in pulleys:
+ratios = []
+
+for a in pulleys:
+    for b in pulleys:
+        for c in pulleys:
+            for d in pulleys:
                 try:
-                    a = Fraction(a_pulley, b_pulley)
-                    b = Fraction(c_pulley, d_pulley)
-                    d = 2/(1 - a * b)
-                    r = float(d)
-                    if r.is_integer() and int(r) in limits:
-                        print(f'[{a_pulley}/{b_pulley}, {c_pulley}/{d_pulley}: {r}')
+                    o = BeltRatio(a, b, c, d)
+                    r = float(o.ma)
+                    if r.is_integer() and int(r) in limits and o not in ratios:
+                        ratios.append(o)
                 except Exception:
                     pass
 
+ratios.sort( key=lambda a: a.ma )
+
+for ratio in ratios:
+    print(ratio)
 
